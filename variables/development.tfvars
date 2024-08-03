2c182483cf46fb236fc8d48ff67bd536a48e3160
Sai Siva Krishna Yenduri <me.sivayenduri@gmail.com>
2024-08-03 22:20:28 +0530

    vars, attributes, cross-reference-attributes

---
+++ variables/development.tfvars
@@ -0,0 +1,2 @@
+ami = "t2.micro"
+instance_type = "ami-0b72821e2f351e396"
---
+++ variables/variables.tf
@@ -0,0 +1,7 @@
+variable "instance_type" {
+    description = "This defines the instance type"
+}
+
+variable "ami" {
+    description = "This defines the usage of ami"
+}
