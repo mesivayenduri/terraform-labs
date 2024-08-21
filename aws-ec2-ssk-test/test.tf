d641e98f40cab541952e66becd5782aafa23eb00
Sai Siva Krishna Yenduri <me.sivayenduri@gmail.com>
2024-08-22 00:48:06 +0530

    Tested published terraform ec2 module

--- aws-ec2-ssk-test/test.tf
+++ aws-ec2-ssk-test/test.tf
@@ -1,11 +1,24 @@
-module "aws-ec2-ssk" {
-    source = "git@github.com:mesivayenduri/aws-ec2-ssk.git"
+# module "aws-ec2-ssk" {
+#     source = "git@github.com:mesivayenduri/aws-ec2-ssk.git"
+# }
+
+# output "instance_creationDate" {
+#     value = module.aws-ec2-ssk.creationDate
+# }
+
+# output "publicip" {
+#     value = module.aws-ec2-ssk.public_ip
+# }
+
+module "ec2" {
+  source  = "mesivayenduri/ec2/aws"
+  version = "0.0.1"
 }
 
 output "instance_creationDate" {
-    value = module.aws-ec2-ssk.creationDate
+    value = module.ec2.creationDate
 }
 
 output "publicip" {
-    value = module.aws-ec2-ssk.public_ip
+    value = module.ec2.public_ip
 }
