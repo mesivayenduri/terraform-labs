1eebbd4750dd59c854116f51314345d4d14b0f51
Sai Siva Krishna Yenduri <me.sivayenduri@gmail.com>
2024-08-23 13:26:27 +0530

    import

--- import/main.tf
+++ import/main.tf
@@ -6,3 +6,9 @@ import {
   to = aws_security_group.mysg
   id = "sg-0a68bab59a12d9cff"
 }
+
+
+
+# If you use terraform plan -generate-config-out=mysg.tf... This will throw an error saying  Error: Too Many Command line arguments.
+
+# Command: terraform plan -generate-config-out="mysg.tf"
